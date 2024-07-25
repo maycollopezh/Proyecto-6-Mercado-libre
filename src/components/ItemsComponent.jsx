c9d99d84290415ffd1f81cc2cd86a8a95769ba17
Maycol Lopez <maycolnlh@gmail.com>
2024-07-24 22:10:36 -0400

    primer commit

---
+++ src/components/ItemsComponent.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+const ItemsComponent = () => {
+
+    const [itemsInfo, setItemsInfo] = useState();
+
+    const API_URL = import.meta.env.VITE_API_URL
+
+    const requestItems = async() => {
+        try {
+            
+            const response = await axios.get (`${API_URL}/items`);
+
+            setItemsInfo(response.data);
+
+            console.log(response.data);
+
+        } catch (error) {
+            console.log('A ocurrido un error al solicitar la información de la API', error);
+        }
+    }
+
+    return(
+        <>
+            <h1>Welcome To MARTEK LP</h1>
+
+            <button onClick={requestItems}>Información de los productos</button>
+        </>
+    )    
+}
+
+export default ItemsComponent;
